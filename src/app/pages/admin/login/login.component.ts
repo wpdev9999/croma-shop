ec588606ece323b46da2006f904369722f3aaf42
wpdev9999 <wpdev9999@gmail.com>
2024-08-27 16:50:43 +0530

    Insitial commit

--- src/app/pages/admin/login/login.component.ts
+++ src/app/pages/admin/login/login.component.ts
@@ -4,12 +4,13 @@ import { ReactiveFormsModule, FormGroup, FormControl, Validators } from '@angula
 import { MatFormFieldModule } from '@angular/material/form-field'; 
 import { MatInputModule } from '@angular/material/input'; 
 import { MatButtonModule } from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
 import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule, MatFormFieldModule, MatInputModule, MatButtonModule],
+  imports: [CommonModule, ReactiveFormsModule, MatFormFieldModule, MatInputModule, MatButtonModule, MatIconModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
@@ -17,6 +18,10 @@ export class LoginComponent {
   
   constructor(private elementRef: ElementRef, private toastr: ToastrService) {}
 
+  toggleRightPanel() {
+    const container = this.elementRef.nativeElement.querySelector('#container');
+    container.classList.toggle('right-panel-active');
+  }
 
   loginForm: FormGroup = new FormGroup({
     username: new FormControl('',[Validators.required]),
@@ -34,10 +39,24 @@ export class LoginComponent {
 
   get l(): any { return this.loginForm.controls; }
   
-  toggleRightPanel() {
-    const container = this.elementRef.nativeElement.querySelector('#container');
-    container.classList.toggle('right-panel-active');
-  }
   
+
+  newUserForm: FormGroup = new FormGroup({
+    username: new FormControl('',[Validators.required]),
+    email: new FormControl('',[Validators.required]),
+    password: new FormControl('',[Validators.required]),
+  });
+
+
+  get r(): any { return this.newUserForm.controls; }
+  
+  signupFormSubmit() {
+    if (this.newUserForm.valid) {
+      this.toastr.success('Registration Successfull!');
+    }
+    else{
+      this.toastr.error('Something went wrong!');
+    }
+  }
 
 }
